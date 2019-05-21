21eec8c0032f8cefc8efb5702d97cafe1cc0bca6
devoptimist <sbrown@chef.io>
2019-05-21 18:57:59 +0100

    update chef workstation url

--- templates/install_ws.sh
+++ templates/install_ws.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo yum install -y vim git
-sudo rpm -Uhv https://packages.chef.io/files/stable/chef-workstation/${chef_workstation_version}/el/7/chef-workstation-${chef_workstation_version}-1.el7.x86_64.rpm
+sudo rpm -Uhv https://packages.chef.io/files/stable/chef-workstation/${chef_workstation_version}/el/7/chef-workstation-${chef_workstation_version}-1.el6.x86_64.rpm
 curl https://raw.githubusercontent.com/habitat-sh/habitat/master/components/hab/install.sh | sudo bash -s -- -v ${hab_version}
 sudo hab license accept
 hab license accept
